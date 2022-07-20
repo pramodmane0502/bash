9d9b6a7cbe772da9540b6a48f7c601d2f08e2f07
pramod <pramod.mane.520@gmail.com>
2022-07-20 10:35:17 +0530

    First Commit

---
+++ src/StaticMethod.java
@@ -0,0 +1,21 @@
+
+
+public class StaticMethod  {
+	
+	public static void me1() {
+		
+		
+		System.out.println("I am learning Static Method");
+			
+		}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		me1();
+		
+	}
+ 
+}
---
+++ src/StaticMethod2.java
@@ -0,0 +1,21 @@
+
+
+public class StaticMethod2  {
+	
+	public static void me1() {
+		
+		
+		System.out.println("I am learning Static Method");
+		System.out.println("I am learning Static Method 2");	
+		}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		me1();
+		
+	}
+ 
+}
